c7b0b2bd5931ddd616563d3f48f98b704ccc1246
DARSHAN PAWAR <dp7878038@gmail.com>
2026-04-30 10:50:56 +0530

    darshan

--- HRMS-PR/src/screen/home.jsx
+++ HRMS-PR/src/screen/home.jsx
@@ -17,29 +17,30 @@ const Home = () => {
             <div className='' style={{ width: "100%", height: "50%" }}>
                 <div className="content ">
                     <h1>Welcome To Our Company</h1>
+                    <h1>Darshan Mart</h1>
                     {/* <p>Aapka content yahan aayega.</p> */}
                 </div>
                 <div className='d-flex gap-5 justify-content-center' style={{width:"100%"}}>
                     <div className=''>
                         <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
-                            <div class="card-body d-flex flex-column p-4">
-                                <h5 class="card-title text-center text-white">Admin</h5>
+                            <div class="card-body d-flex flex-column p-5">
+                                <h5 class="card-title text-center text-white fs-2">Admin</h5>
                                 <button className='btn btn-primary '>Go to</button>
                             </div>
                         </div>
                     </div>
                     <div className=''>
                         <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
-                            <div class="card-body d-flex flex-column p-4">
-                                <h5 class="card-title text-center text-white">Admin</h5>
+                            <div class="card-body d-flex flex-column p-5">
+                                <h5 class="card-title text-center text-white fs-2">Manager</h5>
                                 <button className='btn btn-primary '>Go to</button>
                             </div>
                         </div>
                     </div>
                     <div className=''>
                         <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
-                            <div class="card-body d-flex flex-column p-4">
-                                <h5 class="card-title text-center text-white">Admin</h5>
+                            <div class="card-body d-flex flex-column p-5">
+                                <h5 class="card-title text-center text-white fs-2">Employee</h5>
                                 <button className='btn btn-primary '>Go to</button>
                             </div>
                         </div>
