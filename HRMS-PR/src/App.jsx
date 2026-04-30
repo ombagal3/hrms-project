c3f623a49778370e7e86e0ca03a33437014c5a7f
DARSHAN PAWAR <dp7878038@gmail.com>
2026-04-30 15:02:42 +0530

    darshan

--- HRMS-PR/src/App.jsx
+++ HRMS-PR/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from "./screen/home.jsx" 
 import { Routes,Route } from 'react-router'
+import Admin from './screen/admin.jsx'
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<Admin />}/>
       </Routes>
     </div>
   )
