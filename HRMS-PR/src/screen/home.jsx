917453e2d23616e7964155bad9ea2c6c5e185323
DARSHAN PAWAR <dp7878038@gmail.com>
2026-04-30 10:37:50 +0530

    darshan

--- HRMS-PR/src/screen/home.jsx
+++ HRMS-PR/src/screen/home.jsx
@@ -2,25 +2,52 @@ import React from 'react';
 import './home.css';
 
 const Home = () => {
-  return (
-    <div className="home-container">
-      {/* Background Bubbles */}
-      <div className="bubbles-wrapper">
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-      </div>
+    return (
+        <div className="home-container">
+            {/* Background Bubbles */}
+            <div className="bubbles-wrapper">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+            </div>
 
-      <div className="content">
-        <h1>Welcome To Our Company</h1>
-        {/* <p>Aapka content yahan aayega.</p> */}
-        
-      </div>
-    </div>
-  );
+            <div className='' style={{ width: "100%", height: "50%" }}>
+                <div className="content ">
+                    <h1>Welcome To Our Company</h1>
+                    {/* <p>Aapka content yahan aayega.</p> */}
+                </div>
+                <div className='d-flex gap-5 justify-content-center' style={{width:"100%"}}>
+                    <div className=''>
+                        <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
+                            <div class="card-body d-flex flex-column p-4">
+                                <h5 class="card-title text-center text-white">Admin</h5>
+                                <button className='btn btn-primary '>Go to</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
+                            <div class="card-body d-flex flex-column p-4">
+                                <h5 class="card-title text-center text-white">Admin</h5>
+                                <button className='btn btn-primary '>Go to</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div class="card mt-5 bg-transparent border border-4 rounded-4" style={{ width: "18rem", borderColor: "white" }}>
+                            <div class="card-body d-flex flex-column p-4">
+                                <h5 class="card-title text-center text-white">Admin</h5>
+                                <button className='btn btn-primary '>Go to</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
